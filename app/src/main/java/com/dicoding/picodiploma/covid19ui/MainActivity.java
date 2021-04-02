acaa9e902291c4f405a134574df3f25a691a5b10
hanaaprillia <hanaaprillia990@gmail.com>
2021-04-02 16:35:51 +0700

    first commit

---
+++ app/src/main/java/com/dicoding/picodiploma/covid19ui/MainActivity.java
@@ -0,0 +1,30 @@
+package com.dicoding.picodiploma.covid19ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    ConstraintLayout confirmed;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        confirmed = findViewById(R.id.confirm);
+
+        confirmed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(MainActivity.this, Details.class);
+                startActivity(i);
+            }
+        });
+    }
+}
